d816f3c61dd4c1eb8e0a4f17a23ba47430d37aa2
Nils Siegle <nils.siegle@rennschmiede-pforzheim.de>
2018-12-05 17:46:13 +0100

    Rewrite of base script for smooth transitions

--- advent.sh
+++ advent.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-gpio -g mode 17 out
-gpio -g mode 5 out
-gpio -g mode 16 out
-gpio -g mode 18 out
-
-gpio -g write 17 0
-gpio -g write 5 0
-gpio -g write 16 0
-gpio -g write 18 0
 
 case $1 in
+
+        # 1. Advent
         1) gpio -g write 17 1
+        gpio -g write 5 0
+        gpio -g write 16 0
+        gpio -g write 18 0
         ;;
 
+        # 2. Advent
         2) gpio -g write 17 1
         gpio -g write 5 1
+        gpio -g write 16 0
+        gpio -g write 18 0
         ;;
 
+        # 3. Advent
         3) gpio -g write 17 1
         gpio -g write 5 1
         gpio -g write 16 1
+        gpio -g write 18 0
         ;;
 
+        # 4. Advent
         4) gpio -g write 17 1
         gpio -g write 5 1
         gpio -g write 16 1
         gpio -g write 18 1
+        ;;
+
+        # Default: turn off all LEDs
+        *) gpio -g write 17 0
+        gpio -g write 5 0
+        gpio -g write 16 0
+        gpio -g write 18 0
         ;;
 
 esac
